1c63383bb7830c2eb99bb71a1a21763d1bea35f1
Belén Almendros <belen.almendros19@gmail.com>
2024-08-15 22:52:57 -0400

    terminado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+//tooltip
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  });
+
+  //alert
+  document.getElementById('alertButton').addEventListener('click', function() {
+    alert('El mensaje fue enviado con éxito');
+  });
